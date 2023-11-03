6cbf9d4bb8e0eb55cde2d8a6c424888f0f1342e6
gary wallace <garyatgreatvacation4u@gmail.com>
2023-11-03 11:41:13 -0400

    first push api test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "newmasterconnect.herokuapp.com,www.masterconnect.ca,ww.master-connect.ca,www.garymasterconnect.com,cdn.jsdelivr.net,compute-1.amazonaws.com,master-sale.herokuapp.com,awsprismabucket105646-dev.s3.amazonaws.com,localhost:3000",
+          },
 
-module.exports = nextConfig
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version,XSRF-TOKEN,Access-Control-Allow-Origin",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "new-master.s3.ca-central-1.amazonaws.com",
+        port: "",
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: "https",
+        hostname: "masterultils.com",
+        port: "",
+        // pathname: '/account123/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
